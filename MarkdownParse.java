6973b03fb84711dc87bed83ba7a8f2141cf94ccf
z-h-o-o-o-o-u <108480768+z-h-o-o-o-o-u@users.noreply.github.com>
2022-07-07 09:52:16 -0700

    fixed all the bugs

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,5 +1,4 @@
 //https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,16 +10,33 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        int openBracket;
+        int closeBracket;
+        int openParen;
+        int closeParen;
         while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            openBracket = markdown.indexOf("[", currentIndex);
+            closeBracket = markdown.indexOf("]", openBracket);
+            openParen = markdown.indexOf("(", closeBracket);
+            closeParen = markdown.indexOf(")", openParen);
+            if (openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1){return toReturn;}
+            else if (hasNewLine(markdown.substring(openBracket, closeBracket))){currentIndex = openBracket; continue;}
+            else if (openParen - 1 != closeBracket){currentIndex = closeBracket; continue;}
+            else if (hasSpace(markdown.substring(openParen, closeParen))){currentIndex = closeParen; continue;}
+            else if (openBracket != 0 && markdown.charAt(openBracket - 1) == '!'){currentIndex = closeParen; continue;}
+            else {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;}
         }
-
         return toReturn;
+    }
+
+    public static boolean hasNewLine(String sub) {
+        return sub.contains("\n");
+    }
+
+    public static boolean hasSpace(String sub) {
+        return sub.contains("\n") || sub.contains(" ");
     }
 
 
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,81 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MarkdownParseTest {
 
+    public static String loadfile(String path) throws IOException {
+        String file = Files.readString(Path.of(path));
+        //"/Users/zhou/Documents/GitHub/markdown-parser-1/", 
+        return file;
+    }
+    
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    public void test1() throws IOException{
+        List<String> l = List.of("https://something.com", "some-thing.html");
+        String file = loadfile("test-file.md");
+        assertEquals(l, MarkdownParse.getLinks(file));
+    }
+
+    @Test
+    public void test2() throws IOException{
+        List<String> l = List.of("https://google.com", "some-thing.html");
+        String file = loadfile("test-file2.md");
+        assertEquals(l, MarkdownParse.getLinks(file));
+    }
+
+    @Test
+    public void test3() throws IOException{
+        List<String> l = List.of();
+        String file = loadfile("test-file3.md");
+        assertEquals(l, MarkdownParse.getLinks(file));
+    }
+
+    @Test
+    public void test4() throws IOException{
+        List<String> l = List.of();
+        String file = loadfile("test-file4.md");
+        assertEquals(l, MarkdownParse.getLinks(file));
+    }
+
+    @Test
+    public void test5() throws IOException{
+        List<String> l = List.of();
+        String file = loadfile("test-file5.md");
+        assertEquals(l, MarkdownParse.getLinks(file));
+    }
+    
+    @Test
+    public void test6() throws IOException{
+        List<String> l = List.of();
+        String file = loadfile("test-file6.md");
+        assertEquals(l, MarkdownParse.getLinks(file));
+    }
+
+    @Test
+    public void test7() throws IOException{
+        List<String> l = List.of();
+        String file = loadfile("test-file7.md");
+        assertEquals(l, MarkdownParse.getLinks(file));
+    }
+
+    @Test
+    public void test8() throws IOException{
+        List<String> l = List.of();
+        String file = loadfile("test-file8.md");
+        assertEquals(l, MarkdownParse.getLinks(file));
+    }
+    
+
 
 }
